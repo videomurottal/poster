74d6b4d3550d90e56b053260adb16e216ec02c56
Tafsir Netlify App <tafsir.netlify.app@gmail.com>
2025-07-17 02:05:22 +0700

    Update fetchPrivateAuth.js

--- netlify/functions/fetchPrivateAuth.js
+++ netlify/functions/fetchPrivateAuth.js
@@ -1,7 +1,7 @@
 // netlify/functions/fetchPrivateAuth.js
 exports.handler = async function () {
   const GITHUB_TOKEN = process.env.TOKEN_AUTH; // Simpan di Netlify Env
-  const repo = 'username/private-repo';        // REPO PRIVAT
+  const repo = 'tafsirnetlifyapp/privateauth';        // REPO PRIVAT
   const filePath = 'private_auth.json';
 
   const url = `https://api.github.com/repos/${repo}/contents/${filePath}`;
