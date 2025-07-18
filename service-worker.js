81ae5d458121420d3bfccc6a9c6fbdd6a86367de
Tafsir Netlify App <tafsir.netlify.app@gmail.com>
2025-07-18 14:53:18 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'poster-quran-v3'; // 🔥 Ganti versi biar refresh
+const CACHE_NAME = 'poster-quran-v4'; // 🔥 Ganti versi biar refresh
 const urlsToCache = [
   '/',
   '/index.html',
