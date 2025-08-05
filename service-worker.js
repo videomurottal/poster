e9d61f6cdd7b71c5a23db1fa2f6265d1d3d060f4
Video Murottal <videomurottal.com@gmail.com>
2025-08-05 14:54:47 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'poster-quran-v16'; // 🔥 Ganti versi biar refresh
+const CACHE_NAME = 'poster-quran-v17'; // 🔥 Ganti versi biar refresh
 const urlsToCache = [
   '/',
   '/index.html',
